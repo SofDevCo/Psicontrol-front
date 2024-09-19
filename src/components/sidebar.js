74fbd048f749fcc65245fc294110d6de995d55db
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-09-19 16:59:24 -0300

    fix(front):"fix update css for Sidebar"

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import logo from '../images/Psicontrol.png'
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../images/Psicontrol.png';
+import { Home, RecDesp, User, Config } from '../icons/icons';
 
 const Sidebar = () => {
-    return(
-        <aside class="w-[250px] bg-gray-200 text-gray-800 p-5">
-                <div>
-                <img src={logo} alt="Logo" class="mb-[78px] w-64 mb-4" />
-                    <nav>
-                        <ul>
-                            <li class="mb-[40px]"><a href="#dashboard">Dashboard</a></li>
-                            <li class="mb-[40px]"><a href="/customers">Pacientes</a></li>
-                            <li class="mb-[40px]"><a href="#despesas">Despesas e receitas</a></li>
-                            <li class="mb-[40px]"><a href="#configuracoes">Minhas Configurações</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </aside>
+    const location = useLocation();
+
+    return (
+        <aside className="flex flex-col w-[250px] h-screen bg-white text-gray-800 p-5">
+            <div>
+                <img src={logo} alt="Logo" className="mb-[78px] w-64 mb-4" />
+                <nav className="text-right">
+                    <ul className="mt-[100px]">
+                        <li className={`side-menu mb-[40px] ${location.pathname === '/create-event-form' ? 'ativo' : ''}`}>
+                            <Link to="/create-event-form" className={`flex items-center ${location.pathname === '/create-event-form' ? 'active' : ''}`}>
+                                <Home/>
+                                <span className="py-1 text-2xl">Dashboard</span>
+                            </Link>
+                        </li>
+                        <li className={`side-menu mb-[40px] ${location.pathname === '/income' ? 'ativo' : ''}`}>
+                            <Link to="/income" className={`flex items-center ${location.pathname === '/income' ? 'active' : ''}`}>
+                                <RecDesp/>
+                                <div className="text-2xl leading-none">
+                                    <span className="flex">Receitas e</span>
+                                    <span className="flex">Despesas</span>
+                                </div>
+                            </Link>
+                        </li>
+                        <li className={`side-menu mb-[40px] ${location.pathname === '/customers' ? 'ativo' : ''}`}>
+                            <Link to="/customers" className={`flex items-center ${location.pathname === '/customers' ? 'active' : ''}`}>
+                                <User/>
+                                <span className="text-2xl">Pacientes</span>
+                            </Link>
+                        </li>
+                        <li className={`side-menu mb-[40px] ${location.pathname === '/configuracoes' ? 'ativo' : ''}`}>
+                            <Link to="/configuracoes" className={`flex items-center ${location.pathname === '/configuracoes' ? 'active' : ''}`}>
+                                <Config/>
+                                <div className="text-2xl leading-none">
+                                    <span className="flex">Minhas</span>
+                                    <span className="block">Configurações</span>
+                                </div>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
     );
 }
 
