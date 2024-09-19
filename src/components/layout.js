c60bd3dfd076943e4cfe11a73e4b3ef4bf45995f
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-09-19 16:57:29 -0300

    feat(front):"create file layout in componentes  for import TopBar and Sidebar"

---
+++ src/components/layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import TopBar from "./topbar";
+import Sidebar from "./sidebar";
+
+const Layout = () => {  
+    return (
+        <div className="layout-container flex h-screen">
+            <Sidebar />
+            <div className="main-content flex flex-col w-full overflow-auto"> 
+                <TopBar />
+                <main className="flex-1 p-6">
+                    <Outlet /> 
+                </main>
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
