75f87a83d61ea856f4c3f2f354770a2f259e53b1
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-09-24 17:31:44 -0300

    fix(front):"change color background for bg-primaria"

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ const Layout = () => {
     return (
         <div className="layout-container flex h-screen">
             <Sidebar />
-            <div className="main-content flex flex-col w-full overflow-auto bg-little-blue"> 
+            <div className="main-content flex flex-col w-full overflow-auto bg-clara4"> 
                 <TopBar />
                 <main className="flex-1 p-6">
                     <Outlet /> 
