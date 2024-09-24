8eb7ca129f2a46a643be4d7f1e423812af62cdfe
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-09-24 17:32:15 -0300

    feat(front):"Add new colors"

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'custom-blue': '#0082BA',
+        'primaria': '#0082BA',
         'custom-white': '#DFEAF2',
         'custom-gray': '#8BA3CB',
-        'new-white':'#F5F5F5',
+        'bg1':'#F5F5F5',
         'new-gray': '#81A0AE',
-        'little-blue': '#E9F6F9',
-        'primaria-clara3' : '#BDE3ED'
-      },
-      borderRadius: {
-        'custom': '25px'
+        'clara4': '#EDF5FC',
+        'clara3' : '#C7E0F7',
+        'cinza6': '#81A0AE',
+        'cinza8':'#B6CAD3'
       },
     },
   },
