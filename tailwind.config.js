ceb3924ab49fb17c532309e26d1b5294203668dd
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-09-24 11:49:25 -0300

    feat(front):"Add new modules for tailwind"

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,16 @@ module.exports = {
     extend: {
       colors: {
         'custom-blue': '#0082BA',
-        'custom-white': '#DFEAF2'
+        'custom-white': '#DFEAF2',
+        'custom-gray': '#8BA3CB',
+        'new-white':'#F5F5F5',
+        'new-gray': '#81A0AE',
+        'little-blue': '#E9F6F9',
+        'primaria-clara3' : '#BDE3ED'
       },
       borderRadius: {
         'custom': '25px'
-      }
+      },
     },
   },
   plugins: [],
