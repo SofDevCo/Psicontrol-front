87d13f0a2126f2521815ae31c125026040c6b269
Fabiano-fnc <fabiano_fnc2003@hotmail.com>
2024-10-02 18:16:09 -0300

    feat(add): success modal added

--- src/pages/IncomePage.js
+++ src/pages/IncomePage.js
@@ -351,6 +351,7 @@ const IncomePage = () => {
           </div>
         </div>
       )}
+      
       {isSuccessModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-6 rounded-md shadow-lg">
