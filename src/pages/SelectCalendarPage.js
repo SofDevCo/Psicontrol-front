1fa7d8c2167f5cfe52e0002a2598fb4c58addbad
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-10-07 15:01:20 -0300

    feat(front):"modeling the select calendars page according to figma"

--- src/pages/SelectCalendarPage.js
+++ src/pages/SelectCalendarPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../images/Psicontrol.png";
 import "../index.css";
+import { CalendarIcon } from "../icons/icons";
 
 const SelectCalendarPage = () => {
   const [calendars, setCalendars] = useState([]);
@@ -52,76 +53,51 @@ const SelectCalendarPage = () => {
   };
 
   return (
-    <div className="max-w-[900px] mx-auto p-8 font-sans text-center flex flex-col items-center">
-      <img src={logo} alt="Logo" className="block mx-auto mb-8 w-[150px]" />
-      <h1 className="text-primaria mb-8 text-[32px]">
-        Selecione as agendas que gostaria de utilizar
-      </h1>
+    <div className="flex flex-col items-center justify-center h-screen mx-auto  p-8 bg-bg1 text-center font-sans">
+      <img
+        src={logo}
+        alt="Logo"
+        className="mx-auto mb-8 w-[275px]  h-[165px]"
+      />
       {loading ? (
-        <p className="text-gray-500 italic">Carregando calendários...</p>
+        <p className="italic text-gray-500">Carregando calendários...</p>
       ) : error ? (
-        <p className="text-gray-500 italic">{error}</p>
+        <p className="italic text-gray-500">{error}</p>
       ) : (
-        <div className="flex justify-center items-start gap-8 mt-8 flex-wrap ml-[136px]">
-          <div className="flex flex-col items-start justify-center w-auto p-[5px] max-w-[250px]">
-            <h2 className="text-center text-[19.2px] font-bold mb-[16px] ">
-              Minhas Agendas
+        <div className="mt-8 flex flex-col items-center justify-center gap-8">
+          <div className="flex w-auto p-[5px] rounded-[8px] border-2 border-solid border-primaria flex-col items-center justify-center">
+            <h1 className="mb-8 text-[25px] leading-[29px] text-texto1 font-['Ubuntu'] font-normal">
+              Escolher agenda
+            </h1>
+            <h2 className="mb-[16px] border-b border-b-cinza6 text-center text-[19.2px] text-texto1 font-bold flex items-center justify-center">
+              <CalendarIcon />
+              <span>Minhas Agendas</span>
             </h2>
-            {calendars
-              .slice(0, Math.ceil(calendars.length / 2))
-              .map((calendar) => (
-                <div
-                  key={calendar.id}
-                  className="flex items-center mb-2 w-full"
-                >
-                  <input
-                    type="checkbox"
-                    className="mr-2"
-                    name="calendar"
-                    id={calendar.id}
-                    checked={selectedCalendarIds.has(calendar.id)}
-                    onChange={() => handleCheckboxChange(calendar.id)}
-                  />
-                  <label htmlFor={calendar.id} className="font-bold">
-                    {calendar.summary}
-                  </label>
-                </div>
-              ))}
-          </div>
-          <div className="flex flex-col items-start justify-center w-auto p-[5px] max-w-[250px]">
-            <h2 className="text-center text-[19.2px] font-bold mb-[16px] ">
-              Outras Agendas
-            </h2>
-            {calendars
-              .slice(Math.ceil(calendars.length / 2))
-              .map((calendar) => (
-                <div
-                  key={calendar.id}
-                  className="flex items-center mb-2 w-full"
-                >
-                  <input
-                    type="checkbox"
-                    className="mr-2"
-                    name="calendar"
-                    id={calendar.id}
-                    checked={selectedCalendarIds.has(calendar.id)}
-                    onChange={() => handleCheckboxChange(calendar.id)}
-                  />
-                  <label htmlFor={calendar.id} className="font-bold">
-                    {calendar.summary}
-                  </label>
-                </div>
-              ))}
+            {calendars.map((calendar) => (
+              <div key={calendar.id} className="mb-2 flex w-full text-texto2">
+                <input
+                  type="checkbox"
+                  className="mr-2"
+                  name="calendar"
+                  id={calendar.id}
+                  checked={selectedCalendarIds.has(calendar.id)}
+                  onChange={() => handleCheckboxChange(calendar.id)}
+                />
+                <label htmlFor={calendar.id} className="font-bold">
+                  {calendar.summary}
+                </label>
+              </div>
+            ))}
+            <button
+              onClick={handleProceed}
+              className="rounded-[100px] mt-8 block w-[69px] h-[40px] cursor-pointer border-none bg-primaria p-4 text-lg leading-[8px] text-texto4 hover:bg-primaria"
+              disabled={selectedCalendarIds.size === 0}
+            >
+              Ok
+            </button>
           </div>
         </div>
       )}
-      <button
-        onClick={handleProceed}
-        className="block w-52 p-4 mt-8 mx-auto bg-primaria text-white border-none rounded-custom text-lg cursor-pointer transition duration-300 ease-in-out hover:bg-blue-700"
-        disabled={selectedCalendarIds.size === 0}
-      >
-        Salvar
-      </button>
     </div>
   );
 };
