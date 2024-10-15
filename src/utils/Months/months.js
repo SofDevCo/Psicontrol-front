982a56a952dd0177791cdffb796d177698cd3d30
Fabiano-fnc <fabiano_fnc2003@hotmail.com>
2024-10-15 18:43:15 -0300

    fix: removed the svg from the months modal

--- src/utils/Months/months.js
+++ src/utils/Months/months.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useModal } from "../Modal/useModal";
+import { VerifyIcon,CheckIcon ,SetaIcon } from "../../icons/icons";
 
 export const Months = () => {
   const { isModalOpen, openModal, closeModal } = useModal();
@@ -19,18 +20,8 @@ export const Months = () => {
   };
 
   const monthsInRange = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
   ];
 
   const handleMonthClick = (index) => {
@@ -52,106 +43,59 @@ export const Months = () => {
   };
 
   return (
-    <div className="relative">
+    <div className="ml-3 relative top-3">
       <button
         onClick={toggleModal}
-        className="flex items-center justify-between px-4 py-2 w-[200px] bg-blue-600 text-white shadow-md rounded-lg font-semibold"
-      >
+        className="w-[200px] h-[46px] px-3 py-2 bg-[#0082ba] rounded-[10px] shadow justify-start items-center gap-3 inline-flex">
         <div className="flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fillRule="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M5 13l4 4L19 7"
-            />
-          </svg>
-          <span>{`${monthsInRange[selectedMonth]} ${selectedYear}`}</span>
+          <VerifyIcon />
+          <span className="text-neutral-100 text-[21px] font-medium font-['Ubuntu'] tracking-tight">
+            {`${monthsInRange[selectedMonth]}/${selectedYear.toString().slice(-2)}`}
+          </span>
         </div>
-        <span className="ml-1">▼</span>
+        <SetaIcon/>
       </button>
       {isModalOpen && (
-        <div className="flex flex-col items-center mt-2 bg-white shadow-lg rounded-lg p-2 absolute z-10 w-[250px]">
-          <div className="flex items-center justify-between w-full px-4 mb-2">
+        <div className="flex flex-col items-center mt-2 bg-neutral-100 shadow-lg rounded-2xl border border-[#81a0ae] p-2 absolute z-10 w-[250px]">
+          <div className="flex items-center my-3 justify-between w-full px-4 mb-2 border-b border-[#cac4d0]">
             <button
               onClick={() => setShowingMonths(true)}
-              className="text-gray-500 hover:text-black focus:outline-none"
-            >
+              
+              className="text-[#5c5c5c] text-sm font-normal font-['Open Sans'] mb-3 tracking-tight hover:text-black focus:outline-none">
               {monthsInRange[selectedMonth]}
             </button>
             <button
               onClick={() => setShowingMonths(false)}
-              className="text-gray-500 hover:text-black focus:outline-none"
-            >
+              className="text-[#5c5c5c] text-sm font-normal font-['Open Sans'] tracking-tight mb-3 hover:text-black focus:outline-none">
               {selectedYear}
             </button>
           </div>
-          <div className="flex flex-col w-full overflow-y-auto max-h-60">
-            {showingMonths
-              ? monthsInRange.map((month, index) => (
-                  <button
-                    key={index}
-                    onClick={() => handleMonthClick(index)}
-                    className={`w-full px-3 py-2 text-left rounded-md hover:bg-blue-100 ${
-                      selectedMonth === index
-                        ? "bg-blue-200 flex items-center"
-                        : ""
-                    }`}
-                  >
-                    {selectedMonth === index && (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-4 w-4 mr-2"
-                        fillRule="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                    )}
-                    {month}
-                  </button>
-                ))
-              : getYearsRange().map((year) => (
-                  <button
-                    key={year}
-                    onClick={() => handleYearClick(year)}
-                    className={`w-full px-3 py-2 text-left rounded-md hover:bg-blue-100 ${
-                      selectedYear === year
-                        ? "bg-blue-200 flex items-center"
-                        : ""
-                    }`}
-                  >
-                    {selectedYear === year && (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-4 w-4 mr-2"
-                        fillRule="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                    )}
-                    {year}
-                  </button>
-                ))}
+          <div className="flex flex-col w-full overflow-y-auto max-h-60 ">
+            {showingMonths ? (
+              monthsInRange.map((month, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleMonthClick(index)}
+                  className={`w-full px-3 py-2 text-left  rounded-md  hover:bg-blue-100 ${
+                    selectedMonth === index ? "bg-blue-100 flex items-center" : ""
+                  }`}>
+                  {selectedMonth === index && <CheckIcon />}
+                  {month}
+                </button>
+              ))
+            ) : (
+              getYearsRange().map((year) => (
+                <button
+                  key={year}
+                  onClick={() => handleYearClick(year)}
+                  className={`w-full px-3 py-2 text-left rounded-md hover:bg-blue-100 ${
+                    selectedYear === year ? "bg-blue-200 flex items-center" : ""
+                  }`}>
+                  {selectedYear === year && <CheckIcon />}
+                  {year}
+                </button>
+              ))
+            )}
           </div>
         </div>
       )}
