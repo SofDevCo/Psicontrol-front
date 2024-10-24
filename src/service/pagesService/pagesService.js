e3cffd1c1f61c523b1c594ef88f9b39cffbde208
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-10-24 18:54:40 -0300

    feat(service):"add new global route for archive"

--- src/service/pagesService/pagesService.js
+++ src/service/pagesService/pagesService.js
@@ -14,3 +14,19 @@ export const deleteCustomer = async (customerId) => {
       throw new Error("Erro ao tentar deletar");
     }
   };
+
+  export const ArchiveCustomer = async (customerId) => {
+    const response = await fetch(
+      `http://localhost:3000/events/customers/${customerId}/archive`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ archived: true }),
+      }
+    );
+
+    return response;
+  };
