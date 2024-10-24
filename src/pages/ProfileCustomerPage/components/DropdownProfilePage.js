b0b0ea6235ab1421fbb86ecfa51a0d5bd8ac14e5
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-10-24 18:56:23 -0300

    feat(page):"create dropdown for ProfilePage"

---
+++ src/pages/ProfileCustomerPage/components/DropdownProfilePage.js
@@ -0,0 +1,63 @@
+import {
+  HamburguerIcon,
+  EditIcon,
+  Trash,
+  ArchiveIcon,
+} from "../../../icons/icons";
+
+const DropDownProfile = ({
+  dropdownRef,
+  customerId,
+  onDelete,
+  setSelectedPatient,
+  openModal,
+  customers,
+  onArchive,
+}) => {
+  return (
+    <nav
+      className="absolute right-0 top-10 box-border border-[1px] border-solid border-cinza6 bg-bg2 shadow-default z-10"
+      ref={dropdownRef}
+    >
+      <ul className="w-[210px] h-[189px]">
+        <li>
+          <button
+            className="item-center font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
+            onClick={() => {
+              const patient = customers.find(
+                (c) => c.customer_id === customerId
+              );
+              if (patient) {
+                setSelectedPatient(patient);
+              }
+              openModal();
+            }}
+          >
+            <EditIcon />
+            Editar paciente
+          </button>
+        </li>
+        <li>
+          <button
+            className="item-center font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
+            onClick={() => onDelete(customerId)}
+          >
+            <Trash />
+            Excluir paciente
+          </button>
+        </li>
+        <li>
+          <button
+            className="item-center font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
+            onClick={() => onArchive(customerId)}
+          >
+            <ArchiveIcon />
+            ArquivarPaciente
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default DropDownProfile;
