bd201a85be9f8cab34efda52d9dd709a1578abaa
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-10-28 15:41:37 -0300

    feat(pages):"add group and fix dropdown"

--- src/pages/ProfileCustomerPage/components/DropdownProfilePage.js
+++ src/pages/ProfileCustomerPage/components/DropdownProfilePage.js
@@ -11,13 +11,13 @@ const DropDownProfile = ({
 }) => {
   return (
     <nav
-      className="absolute right-0 mt-4 box-border border-[1px] border-solid border-cinza6 bg-bg2 shadow-default"
+      className="absolute right-0 mt-4 box-border border-[1px] border-1 border-cinza6 bg-bg2 shadow-dropShadow rounded-l-[5px]"
       ref={dropdownRef}
     >
       <ul className="w-[210px] h-[189px] p-4">
-        <li>
+        <li>  
           <button
-            className="mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
+            className="group mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
             onClick={() => {
               setSelectedPatient(customer);
               openModal();
@@ -29,7 +29,7 @@ const DropDownProfile = ({
         </li>
         <li>
           <button
-            className="mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
+            className="group mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
             onClick={() => onDelete(customerId)}
           >
             <Trash />
@@ -38,7 +38,7 @@ const DropDownProfile = ({
         </li>
         <li>
           <button
-            className="mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
+            className="group mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
             onClick={() => onArchive(customerId)}
           >
             <ArchiveIcon />
