66529803ab107a8c90f0fa3f78dd29555c488726
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-10-30 15:10:23 -0300

    fix(pages):"Organizing the labels"

--- src/pages/ArchivedPage/components/DropDownArchive.js
+++ src/pages/ArchivedPage/components/DropDownArchive.js
@@ -1,41 +1,41 @@
 import { Trash, UnarchiveIcon } from "../../../icons/icons";
 
 const DropDown = ({
-    dropdownRef,
-    customerId,
-    onDelete,
-    setSelectedPatient,
-    openModal,
-    customers,
-    onUnarchive,
-  }) => {
-    return (
-      <nav
-        className="absolute right-0 box-border border-[1px] border-1 border-cinza6 bg-bg2 shadow-dropShadow rounded-l-[5px]"
-        ref={dropdownRef}
-      >
-        <ul className="w-[210px] h-[130px] p-1 ">
-          <li>
-            <button
-              className="group mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
-              onClick={() => onDelete(customerId)}
-            >
-              <Trash />
-              Excluir paciente
-            </button>
-          </li>
-          <li>
-            <button
-             className="group mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
-             onClick={()=> onUnarchive(customerId)}
-            >
-                <UnarchiveIcon/>
-                Desarquivar Paciente
-            </button>
-          </li>
-        </ul>
-      </nav>
-    );
-  };
-  
-  export default DropDown;
+  dropdownRef,
+  customerId,
+  onDelete,
+  setSelectedPatient,
+  openModal,
+  customers,
+  onUnarchive,
+}) => {
+  return (
+    <nav
+      className="absolute right-0 box-border border-[1px] border-1 border-cinza6 bg-bg2 shadow-dropShadow rounded-l-[5px]"
+      ref={dropdownRef}
+    >
+      <ul className="w-[210px] h-[130px] p-1 ">
+        <li>
+          <button
+            className="group mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
+            onClick={() => onUnarchive(customerId)}
+          >
+            <UnarchiveIcon />
+            Desarquivar
+          </button>
+        </li>
+        <li>
+          <button
+            className="group mt-5 flex items-center gap-2 ml-6 font-['Open Sans'] flex bg-bg2 text-[15px] font-normal not-italic leading-5 tracking-normal text-texto2 underline hover:bg-bg2 hover:text-texto2/50"
+            onClick={() => onDelete(customerId)}
+          >
+            <Trash />
+            Excluir do arquivo
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default DropDown;
