e2a7c7cadb810eda12c6dae87cea25c4ba59be58
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-10-30 15:13:20 -0300

    feat(pages):"adjust styles"

--- src/pages/ProfileCustomerPage/components/notificationProfilePage.js
+++ src/pages/ProfileCustomerPage/components/notificationProfilePage.js
@@ -2,182 +2,8 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SuccessIcon } from "../../../icons/icons";
 
-export const showEditToast = () => {
-  const overlay = document.createElement("div");
-  overlay.style.position = "fixed";
-  overlay.style.top = "0";
-  overlay.style.left = "0";
-  overlay.style.width = "100vw";
-  overlay.style.height = "100vh";
-  overlay.style.backgroundColor = "rgba(51, 184, 209, 0.3)";
-  overlay.style.zIndex = "30"; 
-  overlay.style.backdropFilter = "blur(6px)";
-  document.body.appendChild(overlay);
+export const showDeleteProfileToast = () => {
 
-  toast.success(
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div style={{ marginBottom: "10px"}}>
-        <SuccessIcon style={{ fontSize: "40px", color: "#0091D0" }} />
-      </div>
-      <div style={{ textAlign: "center", fontFamily: "Ubuntu" }}>
-        <span
-          style={{
-            color: "#4F4F4F",
-            fontSize: "21px",
-            fontWeight: "500",
-            letterSpacing: "-0.5px",
-          }}
-        >
-          Alterações
-        </span>
-        <span
-          style={{
-            color: "#0082BA",
-            fontSize: "21px",
-            fontWeight: "500",
-            letterSpacing: "-0.5px",
-          }}
-        >
-          {" "}
-          salvas
-        </span>
-        <div
-          style={{
-            color: "#4F4F4F",
-            fontSize: "21px",
-            fontWeight: "500",
-            letterSpacing: "-0.5px",
-          }}
-        >
-          com sucesso!
-        </div>
-      </div>
-    </div>,
-    {
-      style: {
-        height: "207px",
-        width: "360px",
-        backgroundColor: "#F5F5F5",
-        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-        borderRadius: "8px",
-        border: "1px solid #81A0AE",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        textAlign: "center",
-        marginLeft:"96px",
-        marginTop:"100px",
-      },
-      position: "top-center",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      icon: false,
-      onClose: () => document.body.removeChild(overlay),
-    }
-  );
-};
-
-export const showArchiveToast = () => {
-
-  const overlay = document.createElement("div");
-  overlay.style.position = "fixed";
-  overlay.style.top = "0";
-  overlay.style.left = "0";
-  overlay.style.width = "100vw";
-  overlay.style.height = "100vh";
-  overlay.style.backgroundColor = "rgba(51, 184, 209, 0.3)";
-  overlay.style.zIndex = "30"; 
-  overlay.style.backdropFilter = "blur(6px)";
-  document.body.appendChild(overlay);
-
-  toast.success(
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div style={{ marginBottom: "10px" }}>
-        <SuccessIcon style={{ fontSize: "40px", color: "#0091D0" }} />
-      </div>
-      <div style={{ textAlign: "center", fontFamily: "Ubuntu" }}>
-        <span
-          style={{
-            color: "#4F4F4F",
-            fontSize: "21px",
-            fontWeight: "500",
-            letterSpacing: "-0.5px",
-          }}
-        >
-          Paciente
-        </span>
-        <span
-          style={{
-            color: "#0082BA",
-            fontSize: "21px",
-            fontWeight: "500",
-            letterSpacing: "-0.5px",
-          }}
-        >
-          {" "}
-          arquivado
-        </span>
-        <div
-          style={{
-            color: "#4F4F4F",
-            fontSize: "21px",
-            fontWeight: "500",
-            letterSpacing: "-0.5px",
-          }}
-        >
-          com sucesso!
-        </div>
-      </div>
-    </div>,
-    {
-      style: {
-        height: "207px",
-        width: "360px",
-        backgroundColor: "#F5F5F5",
-        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-        borderRadius: "8px",
-        border: "1px solid #81A0AE",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        textAlign: "center",
-        marginLeft:"96px",
-        marginTop:"100px",
-      },
-      position: "top-center",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      icon: false,
-      onClose: () => document.body.removeChild(overlay),
-    }
-  );
-};
-
-export const showDeleteToast = () => {
   const overlay = document.createElement("div");
   overlay.style.position = "fixed";
   overlay.style.top = "0";
@@ -262,7 +88,7 @@ export const showDeleteToast = () => {
           justifyContent: "center",
           textAlign: "center",
           marginLeft:"96px",
-          marginTop:"100px",
+          marginTop:"120px",
         
         },
 
@@ -280,7 +106,8 @@ export const showDeleteToast = () => {
     );
   };
 
-  export const showSuccessToast = () => {
+  export const showArchiveProfileToast = () => {
+
     const overlay = document.createElement("div");
     overlay.style.position = "fixed";
     overlay.style.top = "0";
@@ -291,7 +118,7 @@ export const showDeleteToast = () => {
     overlay.style.zIndex = "30"; 
     overlay.style.backdropFilter = "blur(6px)";
     document.body.appendChild(overlay);
-
+  
     toast.success(
       <div
         style={{
@@ -324,7 +151,7 @@ export const showDeleteToast = () => {
             }}
           >
             {" "}
-            adicionado
+            arquivado
           </span>
           <div
             style={{
@@ -352,7 +179,7 @@ export const showDeleteToast = () => {
           justifyContent: "center",
           textAlign: "center",
           marginLeft:"96px",
-          marginTop:"100px",
+          marginTop:"120px",
         },
         position: "top-center",
         autoClose: 3000,
@@ -366,6 +193,3 @@ export const showDeleteToast = () => {
       }
     );
   };
-  
-  
-  
