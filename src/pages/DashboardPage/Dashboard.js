91f7b0a5b2c67e7b0090463e019feae6fea36260
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-11-08 14:49:02 -0300

    feat(pages):"dividing the cards into 2"

--- src/pages/DashboardPage/Dashboard.js
+++ src/pages/DashboardPage/Dashboard.js
@@ -7,7 +7,7 @@ const DashBoard = () => {
   const [events, setEvents] = useState([]);
   const [calendars, setCalendars] = useState([]);
   const [selectedCalendarId, setSelectedCalendarId] = useState("");
-  const [patients, setPatients] = useState([]); 
+  const [patients, setPatients] = useState([]);
   const [unmatchedPatients, setUnmatchedPatients] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -91,7 +91,7 @@ const DashBoard = () => {
 
       if (response.ok) {
         const data = await response.json();
-        setUnmatchedPatients(data); 
+        setUnmatchedPatients(data);
       } else {
         console.error("Erro ao buscar pacientes não encontrados");
       }
@@ -125,58 +125,58 @@ const DashBoard = () => {
   }, []);
 
   return (
-    <div className="relative mx-auto mt-12 box-border h-[436px] w-[1076px] rounded-[15px] border-[3px] border-solid border-cinza6 bg-bg1 z-10">
+    <div>
       {loading ? (
         <p>Carregando...</p>
       ) : error ? (
         <p>{error}</p>
       ) : (
         <>
-          <table className="min-w-full bg-white">
-            <thead>
-              <tr>
-                <th className="text-left px-4 py-2">Paciente</th>
-                <th className="text-left px-4 py-2">Valor Consulta</th>
-              </tr>
-            </thead>
-            <tbody>
-              {patients.map((patient, index) => (
-                <tr key={index} className="border-t">
-                  <td className="px-4 py-2">{patient.customer_name}</td>
-                  <td className="px-4 py-2">
-                    R${" "}
-                    {parseFloat(patient.consultation_fee)
-                      .toFixed(2)
-                      .replace(".", ",")}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
-          <h2 className="mt-6 text-lg font-bold">Pacientes não encontrados</h2>
-          <table className="min-w-full bg-white mt-2">
-            <thead>
-              <tr>
-                <th className="text-left px-4 py-2">Nome do Evento</th>
-              </tr>
-            </thead>
-            <tbody>
-              {unmatchedPatients.length > 0 ? (
-                unmatchedPatients.map((event, index) => (
-                  <tr key={index} className="border-t">
-                    <td className="px-4 py-2">{event.name}</td>
-                  </tr>
-                ))
-              ) : (
+          <div className="relative mx-auto box-border h-[436px] w-[1076px] rounded-[15px] border-[3px] overflow-y-auto border-solid border-cinza6 bg-bg1 z-10">
+            <table className="min-w-full bg-bg1">
+              <thead>
                 <tr>
-                  <td colSpan="4" className="text-center px-4 py-2">
-                    Nenhum paciente não encontrado
-                  </td>
+                  <th className="text-left px-4 py-2">Paciente</th>
+                  <th className="text-left px-4 py-2">Valor Consulta</th>
                 </tr>
-              )}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {patients.map((patient, index) => (
+                  <tr key={index}>
+                    <td className="px-4 py-2">{patient.customer_name}</td>
+                    <td className="px-4 py-2">
+                      R${" "}
+                      {parseFloat(patient.consultation_fee)
+                        .toFixed(2)
+                        .replace(".", ",")}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="relative mx-auto mt-32 box-border h-[141px] w-[1076px] rounded-[15px] border-[3px] overflow-y-auto border-solid border-cinza6 bg-bg1 z-10">
+            <h2 className="mt-6 text-lg font-bold">
+              Pacientes não encontrados
+            </h2>
+            <table className="min-w-full bg-bg1 mt-2">
+              <tbody>
+                {unmatchedPatients.length > 0 ? (
+                  unmatchedPatients.map((event, index) => (
+                    <tr key={index} className="border-t">
+                      <td className="px-4 py-2">{event.name}</td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="4" className="text-center px-4 py-2">
+                      Nenhum paciente não encontrado
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
         </>
       )}
     </div>
