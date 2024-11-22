b112ec2b282238e6c83f6457abbf4d9f2606adb0
Fabiano-fnc <fabiano_fnc2003@hotmail.com>
2024-11-22 15:08:11 -0300

    fix(front): "added toastify, switch accounts and activate and deactivate calendar with question before action"

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { CheckMessage, EditIcon } from "../../icons/icons";
+import { CheckMessage, EditIcon, RefreshIcon } from "../../icons/icons";
+import { showAlteredToast } from "./components/toastUserPage";
 
 const UserPage = () => {
   const [userData, setUserData] = useState({
@@ -9,15 +10,18 @@ const UserPage = () => {
     user_cnpj: "",
     crp_number: "",
     user_phone: "",
-    user_message: "", 
-    image: null, 
+    user_message: "",
+    image: null,
   });
 
   const [isEditing, setIsEditing] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [calendars, setCalendars] = useState([]);
   const [selectedCalendars, setSelectedCalendars] = useState(new Set());
   const [isEditingMessage, setIsEditingMessage] = useState(false);
   const [fileName, setFileName] = useState("nomedoarquivo.png");
+  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
+  const [activeCalendarId, setActiveCalendarId] = useState(null);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -61,7 +65,7 @@ const UserPage = () => {
           );
           if (savedSelectedCalendars) {
             const initiallySelected = new Set(savedSelectedCalendars);
-            console.log("Initially selected calendars:", initiallySelected); 
+            console.log("Initially selected calendars:", initiallySelected);
             setSelectedCalendars(initiallySelected);
           }
         } else {
@@ -93,7 +97,7 @@ const UserPage = () => {
     );
 
     const calendar = calendars.find((cal) => cal.id === calendarId);
-    const calendarName = calendar ? calendar.summary : "Nome padrão"; 
+    const calendarName = calendar ? calendar.summary : "Nome padrão";
 
     await fetch(
       `http://localhost:3000/events/calendars/selection/${calendarId}`,
@@ -104,47 +108,65 @@ const UserPage = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          enabled: isEnabled, 
-          calendar_name: calendarName, 
+          enabled: isEnabled,
+          calendar_name: calendarName,
         }),
       }
     );
   };
 
-  const handleSave = async () => {
+  const handleChangeAccount = async () => {
     try {
-      const formData = new FormData();
-      formData.append("user_cpf", userData.user_cpf);
-      formData.append("user_cnpj", userData.user_cnpj);
-      formData.append("crp_number", userData.crp_number);
-      formData.append("user_phone", userData.user_phone);
-      formData.append("user_message", userData.user_message);
+      const response = await fetch("http://localhost:3000/google");
+      const data = await response.json();
 
-      if (userData.image) {
-        formData.append("image", userData.image);
-      }
-
-      const response = await fetch(`http://localhost:3000/user/users`, {
-        method: "PUT",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
-        },
-        body: formData,
-      });
-
-      if (response.ok) {
-        const updatedData = await response.json();
-        setUserData((prevData) => ({
-          ...prevData,
-          ...updatedData,
-        }));
-        setIsEditing(false);
-      } else {
-        console.error("Erro ao salvar os dados do usuário.");
+      if (data.authUrl) {
+        window.location.href = data.authUrl;
       }
     } catch (error) {
-      console.error("Erro ao conectar com o servidor:", error);
+      console.error("Erro ao trocar de conta:", error);
     }
+  };
+
+  const handleSave = async () => {
+    const formData = new FormData();
+    formData.append("user_cpf", userData.user_cpf);
+    formData.append("user_cnpj", userData.user_cnpj);
+    formData.append("crp_number", userData.crp_number);
+    formData.append("user_phone", userData.user_phone);
+    formData.append("user_message", userData.user_message);
+
+    if (userData.image) {
+      formData.append("image", userData.image);
+    }
+
+    const response = await fetch(`http://localhost:3000/user/users`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
+      },
+      body: formData,
+    });
+
+    if (response.ok) {
+      const updatedData = await response.json();
+      setUserData((prevData) => ({
+        ...prevData,
+        ...updatedData,
+      }));
+      setIsEditing(false);
+      showAlteredToast(); // Exibe a Toastify para indicar alterações salvas
+    } else {
+      console.error("Erro ao salvar os dados do usuário.");
+    }
+  };
+
+  const openModalToChangeAccount = () => {
+    setIsModalOpen(true); // Abre o modal de confirmação
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false); // Fecha o modal
   };
 
   const handleChange = (e) => {
@@ -159,24 +181,37 @@ const UserPage = () => {
     const file = e.target.files[0];
     setUserData((prevData) => ({
       ...prevData,
-      image: file, 
+      image: file,
     }));
-    setFileName(file ? file.name : ""); 
+    setFileName(file ? file.name : "");
+  };
+
+  const openConfirmationModal = (calendarId) => {
+    setActiveCalendarId(calendarId);
+    setIsConfirmationModalOpen(true);
+  };
+
+  const closeConfirmationModal = () => {
+    setIsConfirmationModalOpen(false);
+    setActiveCalendarId(null);
+  };
+
+  const handleToggleCalendar = (calendarId) => {
+    toggleCalendar(calendarId); // Sua lógica original
+    closeConfirmationModal();
   };
 
   return (
     <div className="flex flex-col ">
       {isEditing && (
         <div className="fixed inset-0 ml-[195px] backdrop-blur-[6px] bg-[#33B8D14D] bg-opacity-30  flex justify-center items-center z-30">
-          <div className=" w-[1076px] -mb-24 h-[615px] bg-neutral-100 rounded-[15px] border-2 border-[#81a0ae] shadow-lg p-8">
+          <div className=" w-[1076px] -mb-14 h-[615px] mr-16 bg-neutral-100 rounded-[15px] border-2 border-[#81a0ae] shadow-lg p-8">
             <div className="flex gap-8">
-              
               <div className="space-y-6">
                 <h2 className="text-[25px] mx-[25px] font-medium text-[#0082ba] font-['Ubuntu']">
                   Editar meus dados
                 </h2>
 
-                
                 <div>
                   <label className="block mx-[42px] text-base font-normal font-['Open Sans'] text-[#232323] tracking-wide mb-1">
                     Nome
@@ -191,7 +226,6 @@ const UserPage = () => {
                   />
                 </div>
 
-                
                 <div>
                   <label className="block mx-[42px] text-base font-normal font-['Open Sans'] text-[#232323] tracking-wide mb-1">
                     E-mail
@@ -206,7 +240,6 @@ const UserPage = () => {
                   />
                 </div>
 
-                
                 <div className="flex gap-4">
                   <div>
                     <label className="block mx-[42px] text-base font-normal font-['Open Sans'] text-[#232323] tracking-wide mb-1">
@@ -237,7 +270,6 @@ const UserPage = () => {
                   </div>
                 </div>
 
-                
                 <div>
                   <label className="block mx-[42px] text-base font-normal font-['Open Sans'] text-[#232323] tracking-wide mb-1">
                     CRP
@@ -253,13 +285,11 @@ const UserPage = () => {
                 </div>
               </div>
 
-              
               <div className="space-y-6 ml-16">
                 <h2 className="text-[25px] font-medium text-[#0082ba] font-['Ubuntu']">
                   Dados para recibo
                 </h2>
 
-                
                 <div>
                   <label className="block mx-[18px] text-base font-normal font-['Open Sans'] text-[#232323] tracking-wide mb-1">
                     Nome/Clínica
@@ -274,7 +304,6 @@ const UserPage = () => {
                   />
                 </div>
 
-               
                 <div>
                   <label className="block mx-[18px] text-base font-normal font-['Open Sans'] text-[#232323] tracking-wide mb-1">
                     CPF/CNPJ
@@ -289,7 +318,6 @@ const UserPage = () => {
                   />
                 </div>
 
-                
                 <div>
                   <label className="block mx-[18px] text-base font-normal font-['Open Sans'] text-[#232323] tracking-wide mb-1">
                     Importar logotipo
@@ -328,7 +356,6 @@ const UserPage = () => {
               </div>
             </div>
 
-            
             <div className="flex mx-[780px] gap-2 mt-[50px]">
               <button
                 onClick={() => setIsEditing(false)}
@@ -348,21 +375,20 @@ const UserPage = () => {
       )}
 
       <>
-        
-        <div className="relative w-[1076px] mx-[100px] h-auto bg-bg1 shadow p-6 border-2 border-cinza6 rounded-[15px] text-F15 mt-4 mb-3">
+        <div className="relative w-[1076px] mx-[100px] h-auto bg-bg1 p-6 border-2 border-cinza6 rounded-[15px] text-F15 mt-4 mb-3">
           <div className="flex justify-between items-center mb-6">
             <h2 className="text-[#0082ba] text-[25px] font-medium font-['Ubuntu']">
               Meus dados
             </h2>
             <button
               onClick={() => setIsEditing(true)}
-              className="text-[#0082ba] text-sm underline"
+              className="text-[#0082ba] drop-shadow-editShadow text-sm underline flex items-center"
             >
-              Editar dados
+              <div className="mr-1 mt-[-6\px] relative">Editar dados</div>
+              <EditIcon />
             </button>
           </div>
 
-          
           <div className="flex justify-between">
             <div className="flex w-[360px]">
               <div className="w-10 h-10 bg-[#33b8d1] rounded-[20px] flex justify-center items-center">
@@ -405,7 +431,6 @@ const UserPage = () => {
               </div>
             </div>
 
-            
             <div className="w-[316px]">
               <div className="text-black text-xl font-medium font-['Ubuntu'] mb-2">
                 Dados para recibo
@@ -437,29 +462,38 @@ const UserPage = () => {
           </div>
         </div>
 
-        
-        <div className="flex space-x-4">
-          <div className="w-[540px] h-auto bg-white mx-[100px] shadow p-6 border-2 border-cinza6 rounded-[15px]">
+        <div className="flex mx-[100px] space-x-4 items-start">
+          {/* Box "Minhas agendas" */}
+          <div className="w-[540px] h-[370px] bg-bg1 shadow p-6 border-2 border-cinza6 rounded-[15px] overflow-hidden">
             <div className="flex justify-between">
               <h3 className="text-[#0082ba] text-[20px] font-medium">
                 Minhas agendas
               </h3>
-              <button className="text-[#0082ba] text-sm underline">
-                Trocar de conta
+              <button
+                onClick={openModalToChangeAccount}
+                className="text-[#0082ba] text-sm underline flex items-center"
+              >
+                <span className="relative drop-shadow-editShadow -mt-1">
+                  Trocar de conta
+                </span>
+                <span className="ml-2">
+                  <RefreshIcon />
+                </span>
               </button>
             </div>
 
-            <p className="mt-4 font-semibold">{userData.user_name}</p>
-            <p className="text-[#8d8d8d]">
+            <p className="mt-4 ml-10 font-semibold">{userData.user_name}</p>
+            <p className="text-[#8d8d8d] ml-10">
               <span className="text-[#5c5c5c]">E-mail:</span>{" "}
               {userData.user_email}
             </p>
 
-            <div className="mt-6">
+            <div className="mt-6 ml-10">
               <h4 className="text-[#232323] text-lg font-medium">
-                Selecionar agendas
+                Agendas sincronizadas
               </h4>
-              <div className="mt-4 space-y-4">
+
+              <div className="mt-3 space-y-4 max-h-[120px] overflow-y-auto">
                 {calendars.map((calendar) => (
                   <div
                     key={calendar.id}
@@ -468,7 +502,7 @@ const UserPage = () => {
                     <input
                       type="checkbox"
                       checked={selectedCalendars.has(calendar.id)}
-                      onChange={() => toggleCalendar(calendar.id)}
+                      onChange={() => openConfirmationModal(calendar.id)}
                       className={`appearance-none w-5 h-5 rounded-full border-2 transition-colors cursor-pointer ${
                         selectedCalendars.has(calendar.id)
                           ? "bg-[#0082ba] border-[#0082ba] shadow-inner"
@@ -484,7 +518,7 @@ const UserPage = () => {
                     <span
                       className={`font-medium ${
                         selectedCalendars.has(calendar.id)
-                          ? "text-[#5c5c5c]" 
+                          ? "text-[#5c5c5c]"
                           : "text-gray-500 opacity-50"
                       }`}
                     >
@@ -494,29 +528,98 @@ const UserPage = () => {
                 ))}
               </div>
             </div>
+
+            {isModalOpen && (
+              <div className="fixed inset-0 z-30 flex items-center backdrop-blur-[6px] justify-center bg-[#33B8D14D] bg-opacity-30">
+                <div className="w-[335px] h-[202px] bg-white rounded-lg border-2 border-[#81a0ae] p-6 shadow-lg transform translate-x-[117px] translate-y-[-169px]">
+                  <div className="w-full text-center mx-auto mb-12">
+                    <span className="text-[#5c5c5c] text-[21px] font-medium font-['Ubuntu'] tracking-tight">
+                      Você tem certeza que <br />
+                      deseja
+                      <span className="text-[#0082ba]"> trocar de conta? </span>
+                    </span>
+                  </div>
+
+                  <div className="flex justify-around">
+                    <button
+                      onClick={closeModal}
+                      className="h-10 w-[100px] bg-white border border-[#0082ba] text-[#0082ba] rounded-full text-center font-semibold hover:bg-[#e6f4f8]"
+                    >
+                      Não
+                    </button>
+                    <button
+                      onClick={() => {
+                        closeModal();
+                        handleChangeAccount();
+                      }}
+                      className="h-10 w-[100px] bg-[#0082ba] text-white rounded-full text-center font-semibold hover:bg-[#007bb8]"
+                    >
+                      Sim
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {/* Modal de Confirmação */}
+            {isConfirmationModalOpen && (
+              <div className="fixed inset-0 z-30 flex items-center justify-center backdrop-blur-[6px] bg-[#33B8D14D] bg-opacity-30">
+                 <div className="w-[335px] h-[202px] bg-white rounded-lg border-2 border-[#81a0ae] p-6 shadow-lg transform translate-x-[117px] translate-y-[-169px]">
+                  <div className="w-full text-center mx-auto mb-12">
+                    <span className="text-[#5c5c5c] text-[21px] font-medium font-['Ubuntu'] tracking-tight">
+                      Você tem certeza que deseja
+                      <br />
+                      {selectedCalendars.has(activeCalendarId) ? (
+                        <span className="text-[#0082ba]">
+                          desativar a agenda?
+                        </span>
+                      ) : (
+                        <span className="text-[#0082ba]">ativar a agenda?</span>
+                      )}
+                    </span>
+                  </div>
+
+                  <div className="flex justify-around">
+                    <button
+                      onClick={closeConfirmationModal}
+                      className="h-10 w-[100px] bg-white border border-[#0082ba] text-[#0082ba] rounded-full text-center font-semibold hover:bg-[#e6f4f8]"
+                    >
+                      Não
+                    </button>
+                    <button
+                      onClick={() => handleToggleCalendar(activeCalendarId)}
+                      className="h-10 w-[100px] bg-[#0082ba] text-white rounded-full text-center font-semibold hover:bg-[#007bb8]"
+                    >
+                      Sim
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
 
-          
           <div className="flex">
-            <div className="w-[520px] h-auto mx-[-100px] bg-white shadow p-6 border-2 border-cinza6 rounded-[15px]">
+            <div className="w-[520px] h-[370px] mx-[-0px] bg-bg1 p-6 border-2 border-cinza6 rounded-[15px]">
               <div className="flex justify-between items-center mb-4">
                 <h3 className="text-[#0082ba] text-[20px] font-medium">
                   Mensagem de cobrança
                 </h3>
                 {isEditingMessage ? (
                   <button
-                    onClick={() => setIsEditingMessage(false)} 
+                    onClick={() => setIsEditingMessage(false)}
                     className="text-[#0082ba] text-sm"
                   >
                     <CheckMessage />
                   </button>
                 ) : (
-                  <button
-                    onClick={() => setIsEditingMessage(true)}
-                    className="text-[#0082ba] text-sm underline"
-                  >
-                    <EditIcon />
-                  </button>
+                  <div>
+                    <button
+                      onClick={() => setIsEditingMessage(true)}
+                      className="text-[#0082ba] drop-shadow-editShadow  text-sm underline"
+                    >
+                      <EditIcon />
+                    </button>
+                  </div>
                 )}
               </div>
 
@@ -525,7 +628,7 @@ const UserPage = () => {
                   name="user_message"
                   value={userData.user_message}
                   onChange={handleChange}
-                  rows={3} 
+                  rows={3}
                   className="w-full p-4 border border-gray-300 rounded-md resize-none text-[#232323] text-[15px] h-[250px]"
                   placeholder="Escreva sua mensagem de cobrança aqui..."
                 />
