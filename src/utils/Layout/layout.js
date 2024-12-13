85eb8fa27eb7c800d3e64661a36cb1d9d3c97f10
Fabiano-fnc <fabiano_fnc2003@hotmail.com>
2024-12-13 18:51:46 -0300

    feat(fixing login page and others things)

--- src/utils/Layout/layout.js
+++ src/utils/Layout/layout.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import TopBar from "./components/topbar";
 import Sidebar from "./components/sidebar";
 
 const Layout = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen((prevState) => !prevState);
+  };
+
   return (
     <div className="layout-container flex h-screen">
-      <Sidebar />
+      {/* Sidebar com estado controlado */}
+      <Sidebar isOpen={isSidebarOpen} />
+
       <div className="main-content flex flex-col w-full h-screen bg-clara4">
-        <TopBar className="ml-0"/> 
+        {/* Passando a função toggleSidebar para o TopBar */}
+        <TopBar onMenuClick={toggleSidebar} />
         <main className="flex-1 p-6">
           <Outlet />
         </main>
