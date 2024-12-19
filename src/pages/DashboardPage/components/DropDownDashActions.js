12283593a6be2f7fcf9176ea77955e89d6178185
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-12-19 11:54:08 -0300

    feat(pages):"dropdown PartialPaymenet"

--- src/pages/DashboardPage/components/DropDownDashActions.js
+++ src/pages/DashboardPage/components/DropDownDashActions.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const DropDownDashActions = ({ onOpenModal }) => {
+const DropDownDashActions = ({ onOpenModal, onPartialPayment }) => {
   return (
     <nav className="absolute right-0 mt-3 box-border border-[1px] border-solid border-cinza6 bg-bg2 shadow-default rounded-md">
       <ul className="w-[210px] h-auto">
@@ -11,6 +11,16 @@ const DropDownDashActions = ({ onOpenModal }) => {
             className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
           >
             Enviar Cobrança
+          </button>
+        </li>
+      </ul>
+      <ul className="w-[210px] h-auto">
+        <li>
+          <button 
+            onClick={onPartialPayment} 
+            className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            Pagamento Parcial
           </button>
         </li>
       </ul>
