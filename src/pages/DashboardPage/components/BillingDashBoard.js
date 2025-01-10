437490ce49aa6bdd63840787535b099eda8bf6b5
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-01-10 15:29:35 -0300

    fix(pages):"message modal"

--- src/pages/DashboardPage/components/BillingDashBoard.js
+++ src/pages/DashboardPage/components/BillingDashBoard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CloseIconMessageModal } from "./IconsDashBoard";
 
 const BillingDashBoard = ({
   onClose,
@@ -8,37 +9,38 @@ const BillingDashBoard = ({
 }) => {
   return (
     <div className="fixed inset-0 bg-bgM/30 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-bg1 rounded-md p-6 w-[477px] h-[521px]">
-        <h2 className="w-[303px] text-primaria text-[21px] font-medium font-['Ubuntu'] tracking-tight">
-          Enviar mensagem de cobrança
-        </h2>
+      <div className="bg-bg1 rounded-B15 p-6 w-[477px] h-[521px] border-cinza6 border-[3px]">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-center text-primaria text-[21px] font-medium font-ubuntu tracking-tight ml-16 mt-3">
+            Enviar mensagem de cobrança
+          </h2>
+          <button onClick={onClose}>
+            <CloseIconMessageModal />
+          </button>
+        </div>
         <p
-          className="mb-4 w-[403px] h-[347.32px]"
+          className="mb-10 ml-8 w-[403px] text-text1 text-sm font-openSans tracking-tight "
           dangerouslySetInnerHTML={{ __html: message.replace(/\n/g, "<br />") }}
         ></p>
-        <div className="flex justify-between">
+        <div className="w- border-b border-cinza6 mb-12"></div>
+        <div className="flex justify-between ml-8 gap-4">
           <button
             onClick={onSendEmail}
-            className="h-[39px] px-6 py-2.5 bg-bg1 rounded-[100px] border border-primaria justify-center items-center gap-2 inline-flex text-center text-primaria text-sm font-semibold font-['Open Sans'] underline tracking-tight"
+            className="w-full h-[39px] px-auto bg-bg1 rounded-[100px] border border-primaria justify-center items-center gap-12 inline-flex text-center text-primaria text-sm font-semibold font-['Open Sans'] underline tracking-tight"
           >
             Enviar por Email
           </button>
           <button
             onClick={onSendWhatsApp}
-            className="h-[39px] px-6 py-2.5 bg-bg1 rounded-[100px] border border-primaria justify-center items-center gap-2 inline-flex text-center text-primaria text-sm font-semibold font-['Open Sans'] underline tracking-tight"
+            className="w-full h-[39px] px-auto  bg-bg1 rounded-[100px] border border-primaria justify-center items-center gap-2 inline-flex text-center text-primaria text-sm font-semibold font-['Open Sans'] underline tracking-tight"
           >
-            Enviar por WhatsApp
+            Enviar pelo WhatsApp
           </button>
         </div>
-        <button
-          onClick={onClose}
-          className="mt-4 w-full text-center text-sm text-gray-700"
-        >
-          Fechar
-        </button>
       </div>
     </div>
   );
+  
 };
 
 export default BillingDashBoard;
