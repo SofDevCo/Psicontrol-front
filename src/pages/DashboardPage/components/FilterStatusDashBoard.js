eccded8a1f57f0ec82b974aad193a0d3634d9c0a
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-01-14 12:54:39 -0300

    fix(pages)"Filter dados ml"

--- src/pages/DashboardPage/components/FilterStatusDashBoard.js
+++ src/pages/DashboardPage/components/FilterStatusDashBoard.js
@@ -14,7 +14,7 @@ const FilterStatusDashBoard = ({ selectedStatus, onChangeStatus }) => {
 
   return (
     <div className="flex items-center z-30">
-      <div className="flex items-center gap-2 md:-ml-[15px] -ml-[107px] md:mt-0">
+      <div className="flex items-center gap-2 md:-ml-[150px] -ml-[107px] md:mt-0">
         <button
           type="button"
           onClick={() => setIsOpen(!isOpen)}
