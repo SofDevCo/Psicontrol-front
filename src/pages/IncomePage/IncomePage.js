5898ebb5707bb6265794df38f07f63d340ab3de1
Léo Camisa <tpleosuporte@gmail.com>
2025-01-21 10:07:23 -0300

    fix(pages):"adjusted delete toast for mobile"

--- src/pages/IncomePage/IncomePage.js
+++ src/pages/IncomePage/IncomePage.js
@@ -413,9 +413,9 @@ const IncomePage = () => {
     <div className="p-6">
       {isModalOpen && (
         <div className="fixed inset-0 z-30 flex items-center backdrop-blur-[6px] justify-center bg-[#33B8D14D] bg-opacity-30">
-          <div className="w-[335px] h-[202px] bg-white rounded-lg border-2 border-[#81a0ae] p-6 shadow-lg transform translate-x-[117px] translate-y-[-169px]">
+          <div className="w-[90%] max-w-[335px] h-auto bg-white rounded-lg border-2 border-[#81a0ae] p-6 shadow-lg">
             <div className="w-full text-center mx-auto mb-6">
-              <span className="text-[#5c5c5c] text-[21px] font-medium font-['Ubuntu'] tracking-tight">
+              <span className="text-[#5c5c5c] text-[18px] md:text-[21px] font-medium font-['Ubuntu'] tracking-tight">
                 Você tem certeza que <br />
                 deseja
                 <span className="text-[#0082ba]"> excluir </span>
