f6bd25f62aea7529c1cf0ed0857f51b550b4ad4d
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-01-27 14:39:19 -0300

    fix(pages):"change method Delete for PUT in deleteCustomer route"

--- src/service/pagesService/pagesService.js
+++ src/service/pagesService/pagesService.js
@@ -1,69 +1,80 @@
 export const deleteCustomer = async (customerId) => {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/events/customers/${customerId}`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
-          "Content-Type": "application/json",
-        },
-      });
-  
-      return response;
-  };
-
-  export const ArchiveCustomer = async (customerId) => {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}/events/customers/${customerId}/archive`,
-      {
-        method: "PUT",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ archived: true }),
-      }
-    );
-
-    return response;
-  };
-
-  export const fetchCustomerProfile = async (customerId) => {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}/events/customers/${customerId}/profile`,
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
-        },
-      }
-    );
-    return response;
-  };
-
-  export const createOrUpdateCustomer = async (customer, additionalAlternatives, customerId = null) => {
-    const url = customerId
-      ? `${process.env.REACT_APP_API_URL}/events/customers/${customerId}`
-      : `${process.env.REACT_APP_API_URL}/events/create-customer`;
-    const method = customerId ? "PUT" : "POST";
-  
-    const response = await fetch(url, {
-      method,
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/events/customers/${customerId}`,
+    {
+      method: "PUT",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ ...customer, additionalAlternatives }),
-    });
-  
-    return response;
-  };
+      body: JSON.stringify({ deleted: true }),
+    }
+  );
 
-  export const fetchCustomers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/events/customers`, {
+  return response;
+};
+
+export const ArchiveCustomer = async (customerId) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/events/customers/${customerId}/archive`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ archived: true }),
+    }
+  );
+
+  return response;
+};
+
+export const fetchCustomerProfile = async (customerId) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/events/customers/${customerId}/profile`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
+      },
+    }
+  );
+  return response;
+};
+
+export const createOrUpdateCustomer = async (
+  customer,
+  additionalAlternatives,
+  customerId = null
+) => {
+  const url = customerId
+    ? `${process.env.REACT_APP_API_URL}/events/customers/${customerId}`
+    : `${process.env.REACT_APP_API_URL}/events/create-customer`;
+  const method = customerId ? "PUT" : "POST";
+
+  const response = await fetch(url, {
+    method,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ...customer, additionalAlternatives }),
+  });
+
+  return response;
+};
+
+export const fetchCustomers = async () => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/events/customers`,
+    {
       method: "GET",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("authentication_token")}`,
         "Content-Type": "application/json",
       },
-    });
-  
-    return response.json();
-  };
+    }
+  );
+
+  return response.json();
+};
