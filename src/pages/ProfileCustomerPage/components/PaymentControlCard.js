89dbed0247935e253fca72cec33fa23d69326a2c
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-01-30 18:50:02 -0300

    feat(pages):"import filter for control payment card"

--- src/pages/ProfileCustomerPage/components/PaymentControlCard.js
+++ src/pages/ProfileCustomerPage/components/PaymentControlCard.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import {
   VerifyGreenIcon,
   CrossIcon,
+  FilterIcon,
 } from "../../DashboardPage/components/IconsDashBoard";
 import {
   sendEmailMessage,
@@ -16,6 +17,7 @@ import { HamburguerIcon } from "../../../icons/icons";
 import BillingDashBoard from "../../DashboardPage/components/BillingDashBoard";
 import DropDownDashActions from "../../DashboardPage/components/DropDownDashActions";
 import ModalPaymentDash from "../..//DashboardPage/components/ModalPaymentDash";
+import FilterStatusProfilePage from "./FilterStatusProfilePage";
 
 const PaymentControlCard = ({
   billingRecords,
@@ -34,6 +36,9 @@ const PaymentControlCard = ({
   const [billingMessage, setBillingMessage] = useState("");
   const [selectedYear] = useState(new Date().getFullYear());
   const [selectedMonth] = useState(new Date().getMonth() + 1);
+  const [selectedStatus, setSelectedStatus] = useState("");
+  const [filteredBillingRecords, setFilteredBillingRecords] =
+    useState(billingRecords);
   const [loading, setLoading] = useState(false);
 
   const toggleDropdownPatients = (index) => {
@@ -191,7 +196,11 @@ const PaymentControlCard = ({
       updateBillingRecords((prevRecords) =>
         prevRecords.map((record) =>
           record.customer_id === customer_id
-            ? { ...record, payment_amount: parseFloat(paymentAmount), payment_status: "parcial" }
+            ? {
+                ...record,
+                payment_amount: parseFloat(paymentAmount),
+                payment_status: "parcial",
+              }
             : record
         )
       );
@@ -206,6 +215,38 @@ const PaymentControlCard = ({
     setIsPartialPaymentModalOpen(true);
   };
 
+  useEffect(() => {
+    const filteredBillingRecords = () => {
+      if (!billingRecords) return;
+
+      const filtered = billingRecords.filter((record) => {
+        if (selectedStatus.length === 0) return true;
+
+        const matchesPaymentStatus =
+          (selectedStatus.includes("abertor") && !record.payment_status) ||
+          selectedStatus.includes(record.payment_status);
+
+        const matchesInvoiceStatus =
+          (selectedStatus.includes("realizada") && record.sending_invoice) ||
+          (selectedStatus.includes("nao-realizada") && !record.sending_invoice);
+
+        const matchesBillofSaleStatus =
+          (selectedStatus.includes("nao-emitidos") && !record.bill_of_sale) ||
+          (selectedStatus.includes("emitidos") && record.bill_of_sale);
+
+        return (
+          matchesPaymentStatus ||
+          matchesInvoiceStatus ||
+          matchesBillofSaleStatus
+        );
+      });
+
+      setFilteredBillingRecords(filtered);
+    };
+
+    filteredBillingRecords();
+  }, [selectedStatus, billingRecords]);
+
   if (!billingRecords || billingRecords.length === 0) {
     return (
       <p className="text-center text-texto2 italic py-4">
@@ -215,144 +256,163 @@ const PaymentControlCard = ({
   }
 
   return (
-    <div className="flex  md:mt-10 md:auto md:mx-auto justify-center box-border w-full h-[443px] md:rounded-B15 rounded-B10 md:border-[3px] border overflow-x-auto border-solid border-cinza6 bg-bg1 z-10 ">
-      <div className="overflow-x-auto  ">
-        <table className="table-fixed w-full bg-bg1 mt-5 text-left">
-          <thead>
-            <tr>
-              <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
-                Mês
-              </th>
-              <th className="text-center align-middle md:whitespace-nowrap min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
-                Valor Consulta
-              </th>
-              <th className="text-center align-middle hidden md:table-cell min-w-[75px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
-                Dias
-              </th>
-              <th className="text-center align-middle md:whitespace-nowrap min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
-                Nº de consultas
-              </th>
-              <th className=" text-center md:align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-5 md:px-4 py-1 md:py-2">
-                Total
-              </th>
-              <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-3 md:px-4 py-1 md:py-2">
-                Cobrança
-              </th>
-              <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-3 md:px-4 py-1 md:py-2">
-                Pagamento
-              </th>
-              <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-5 md:px-4 py-1 md:py-2">
-                NF
-              </th>
-              <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
-                Ações
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {billingRecords.map((item, index) => (
-              <tr
-                key={index}
-                className="relative text-center border-t border-gray-300"
-              >
-                <td className="text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-auto py-2">
-                  {item.month}
-                </td>
-                <td className="text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
-                  R${" "}
-                  {parseFloat(item.consultation_fee)
-                    .toFixed(2)
-                    .replace(".", ",")}
-                </td>
-                <td className="hidden md:table-cell text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
-                  {item.consultation_days
-                    ? item.consultation_days
-                        .split(", ")
-                        .map(Number)
-                        .sort((a, b) => a - b)
-                        .join(", ")
-                    : "-"}
-                </td>
-                <td className="relative text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2 group">
-                  {item.num_consultations}
-                </td>
-                <td className="text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
-                  R$ {item.total_consultation_fee || "0,00"}
-                </td>
-                <td>
-                  <div className="flex items-center justify-center text-center h-full">
-                    {item.sending_invoice ? <VerifyGreenIcon /> : <CrossIcon />}
-                  </div>
-                </td>
-                <td>
-                  <div className="flex items-center justify-center text-center h-full">
-                    {" "}
-                    {item.payment_status === "pago" ? (
-                      <VerifyGreenIcon />
-                    ) : item.payment_status === "parcial" ? (
-                      <span className="text-texto2 md:text-F15 text-F8 font-semibold font-['Open Sans'] tracking-tight rounded-B15 border-2 border-aviso">
-                        R${" "}
-                        {parseFloat(item.payment_amount || 0)
-                          .toFixed(2)
-                          .replace(".", ",")}
-                      </span>
-                    ) : (
-                      <CrossIcon />
-                    )}{" "}
-                  </div>
-                </td>
-                <td>
-                  <div className="flex items-center justify-center text-center h-full">
-                    {item.bill_of_sale ? <VerifyGreenIcon /> : <CrossIcon />}
-                  </div>
-                </td>
+    <>
+      <div className="flex  md:mt-10 md:auto md:mx-auto justify-center box-border w-full h-[443px] md:rounded-B15 rounded-B10 md:border-[3px] border overflow-x-auto border-solid border-cinza6 bg-bg1 z-10 ">
+        <div className="overflow-x-auto  ">
+          <p className="text-F25 text-primaria font-medium font-ubuntu">
+            Controle de pagamento
+          </p>
 
-                <td className="text-center px-2 md:px-4 py-1 md:py-2">
-                  <button
-                    className="cursor-pointer"
-                    onClick={() => toggleDropdownPatients(index)}
-                  >
-                    <HamburguerIcon />
-                  </button>
-
-                  {isDropdownOpenPatients === index && (
-                    <div className="absolute right-0 shadow-lg rounded p-2 z-20">
-                      <DropDownDashActions
-                        onOpenModal={() => handleOpenModalForBilling(item)}
-                        onPartialPayment={() => handleOpenPartialPayment(item)}
-                        onConfirmedPayment={() => handleConfirmPayment(item)}
-                        onConfirmedBillOfSale={() =>
-                          handleConfirmBillOfSale(item)
-                        }
-                      />
-                    </div>
-                  )}
-                </td>
+          <div className="group flex justify-end items-end mr-14 md:mt-0 md:mb-2 mt-16 z-10">
+            <FilterStatusProfilePage
+              selectedStatus={selectedStatus}
+              onChangeStatus={setSelectedStatus}
+            />
+            <FilterIcon />
+          </div>
+          <table className="table-fixed w-full bg-bg1 mt-5 text-left">
+            <thead>
+              <tr>
+                <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
+                  Mês
+                </th>
+                <th className="text-center align-middle md:whitespace-nowrap min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
+                  Valor Consulta
+                </th>
+                <th className="text-center align-middle hidden md:table-cell min-w-[75px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
+                  Dias
+                </th>
+                <th className="text-center align-middle md:whitespace-nowrap min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
+                  Nº de consultas
+                </th>
+                <th className=" text-center md:align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-5 md:px-4 py-1 md:py-2">
+                  Total
+                </th>
+                <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-3 md:px-4 py-1 md:py-2">
+                  Cobrança
+                </th>
+                <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-3 md:px-4 py-1 md:py-2">
+                  Pagamento
+                </th>
+                <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-5 md:px-4 py-1 md:py-2">
+                  NF
+                </th>
+                <th className="text-center align-middle min-w-[100px] border-b border-b-cinza6 text-primaria md:text-lg text-F8 font-medium tracking-tight px-2 md:px-4 py-1 md:py-2">
+                  Ações
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {isBillingModalOpen && selectedPatient && (
-        <BillingDashBoard
-          onClose={closeBillingModal}
-          onSendWhatsApp={() => handleSendWhatsApp(selectedPatient)}
-          onSendEmail={() => handleSendEmail(selectedPatient)}
-          message={billingMessage}
-        />
-      )}
-      <>
-        {isPartialPaymentModalOpen && selectedPatientForPartialPayment && (
-          <ModalPaymentDash
-            onClose={() => setIsPartialPaymentModalOpen(false)}
-            onSave={handleSavePartialPayment}
-            totalAmount={
-              selectedPatientForPartialPayment.total_consultation_fee
-            }
+            </thead>
+            <tbody>
+              {filteredBillingRecords.map((item, index) => (
+                <tr
+                  key={index}
+                  className="relative text-center border-t border-gray-300"
+                >
+                  <td className="text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-auto py-2">
+                    {item.month}
+                  </td>
+                  <td className="text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
+                    R${" "}
+                    {parseFloat(item.consultation_fee)
+                      .toFixed(2)
+                      .replace(".", ",")}
+                  </td>
+                  <td className="hidden md:table-cell text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
+                    {item.consultation_days
+                      ? item.consultation_days
+                          .split(", ")
+                          .map(Number)
+                          .sort((a, b) => a - b)
+                          .join(", ")
+                      : "-"}
+                  </td>
+                  <td className="relative text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2 group">
+                    {item.num_consultations}
+                  </td>
+                  <td className="text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
+                    R$ {item.total_consultation_fee || "0,00"}
+                  </td>
+                  <td>
+                    <div className="flex items-center justify-center text-center h-full">
+                      {item.sending_invoice ? (
+                        <VerifyGreenIcon />
+                      ) : (
+                        <CrossIcon />
+                      )}
+                    </div>
+                  </td>
+                  <td>
+                    <div className="flex items-center justify-center text-center h-full">
+                      {" "}
+                      {item.payment_status === "pago" ? (
+                        <VerifyGreenIcon />
+                      ) : item.payment_status === "parcial" ? (
+                        <span className="text-texto2 md:text-F15 text-F8 font-semibold font-['Open Sans'] tracking-tight rounded-B15 border-2 border-aviso">
+                          R${" "}
+                          {parseFloat(item.payment_amount || 0)
+                            .toFixed(2)
+                            .replace(".", ",")}
+                        </span>
+                      ) : (
+                        <CrossIcon />
+                      )}{" "}
+                    </div>
+                  </td>
+                  <td>
+                    <div className="flex items-center justify-center text-center h-full">
+                      {item.bill_of_sale ? <VerifyGreenIcon /> : <CrossIcon />}
+                    </div>
+                  </td>
+
+                  <td className="text-center px-2 md:px-4 py-1 md:py-2">
+                    <button
+                      className="cursor-pointer"
+                      onClick={() => toggleDropdownPatients(index)}
+                    >
+                      <HamburguerIcon />
+                    </button>
+
+                    {isDropdownOpenPatients === index && (
+                      <div className="absolute right-0 shadow-lg rounded p-2 z-20">
+                        <DropDownDashActions
+                          onOpenModal={() => handleOpenModalForBilling(item)}
+                          onPartialPayment={() =>
+                            handleOpenPartialPayment(item)
+                          }
+                          onConfirmedPayment={() => handleConfirmPayment(item)}
+                          onConfirmedBillOfSale={() =>
+                            handleConfirmBillOfSale(item)
+                          }
+                        />
+                      </div>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {isBillingModalOpen && selectedPatient && (
+          <BillingDashBoard
+            onClose={closeBillingModal}
+            onSendWhatsApp={() => handleSendWhatsApp(selectedPatient)}
+            onSendEmail={() => handleSendEmail(selectedPatient)}
+            message={billingMessage}
           />
         )}
-      </>
-    </div>
+        <>
+          {isPartialPaymentModalOpen && selectedPatientForPartialPayment && (
+            <ModalPaymentDash
+              onClose={() => setIsPartialPaymentModalOpen(false)}
+              onSave={handleSavePartialPayment}
+              totalAmount={
+                selectedPatientForPartialPayment.total_consultation_fee
+              }
+            />
+          )}
+        </>
+      </div>
+    </>
   );
 };
 
