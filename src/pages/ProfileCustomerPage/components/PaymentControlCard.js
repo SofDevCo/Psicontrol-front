7623968990fa2670d503252b453d816f707f6718
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-01-31 18:05:59 -0300

    feat(pages):"button for render info customer in paymant control card"

--- src/pages/ProfileCustomerPage/components/PaymentControlCard.js
+++ src/pages/ProfileCustomerPage/components/PaymentControlCard.js
@@ -270,12 +270,11 @@ const PaymentControlCard = ({
   }, [selectedStatus, billingRecords]);
 
   const toggleTableSize = () => {
-    setIsTableExpanded(!isTableExpanded);
+    setIsTableExpanded((prev) => !prev);
+    setTimeout(() => {
+      window.dispatchEvent(new Event("resize"));
+    }, 100);
   };
-
-    useEffect(() => {
-      setIsTableExpanded(true);
-    }, []);
 
   if (!billingRecords || billingRecords.length === 0) {
     return (
@@ -335,113 +334,118 @@ const PaymentControlCard = ({
               </tr>
             </thead>
             <tbody>
-              {filteredBillingRecords.map((item, index) => (
-                <tr
-                  key={index}
-                  className="relative text-center border-t border-gray-300"
-                >
-                  <td className="text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-auto py-2">
-                    {item.month
-                      ? `${monthsInRangeShort[parseInt(item.month.split("-")[1], 10) - 1]}/${String(selectedYear).slice(-2)}`
-                      : "Mês inválido"}
-                  </td>
-                  <td className="text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
-                    R${" "}
-                    {parseFloat(item.consultation_fee)
-                      .toFixed(2)
-                      .replace(".", ",")}
-                  </td>
-                  <td className="hidden md:table-cell text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
-                    {item.consultation_days
-                      ? item.consultation_days
-                          .split(", ")
-                          .map(Number)
-                          .sort((a, b) => a - b)
-                          .join(", ")
-                      : "-"}
-                  </td>
-                  <td className="relative text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2 group">
-                    {item.num_consultations}
-                  </td>
-                  <td className="text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
-                    R$ {item.total_consultation_fee || "0,00"}
-                  </td>
-                  <td>
-                    <div className="flex items-center justify-center text-center h-full">
-                      {item.sending_invoice ? (
-                        <VerifyGreenIcon />
-                      ) : (
-                        <CrossIcon />
-                      )}
-                    </div>
-                  </td>
-                  <td>
-                    <div className="flex items-center justify-center text-center h-full">
-                      {" "}
-                      {item.payment_status === "pago" ? (
-                        <VerifyGreenIcon />
-                      ) : item.payment_status === "parcial" ? (
-                        <span className="text-texto2 md:text-F15 text-F8 font-semibold font-['Open Sans'] tracking-tight rounded-B15 border-2 border-aviso">
-                          R${" "}
-                          {parseFloat(item.payment_amount || 0)
-                            .toFixed(2)
-                            .replace(".", ",")}
-                        </span>
-                      ) : (
-                        <CrossIcon />
-                      )}{" "}
-                    </div>
-                  </td>
-                  <td>
-                    <div className="flex items-center justify-center text-center h-full">
-                      {item.bill_of_sale ? <VerifyGreenIcon /> : <CrossIcon />}
-                    </div>
-                  </td>
-
-                  <td className="text-center px-2 md:px-4 py-1 md:py-2">
-                    <button
-                      className="cursor-pointer"
-                      onClick={() => toggleDropdownPatients(index)}
-                    >
-                      <HamburguerIcon />
-                    </button>
-
-                    {isDropdownOpenPatients === index && (
-                      <div
-                        ref={outSideClickRef}
-                        className="absolute -mt-23 ml-40 shadow-lg rounded z-20"
-                      >
-                        <DropDownDashActions
-                          onOpenModal={() => handleOpenModalForBilling(item)}
-                          onPartialPayment={() =>
-                            handleOpenPartialPayment(item)
-                          }
-                          onConfirmedPayment={() => handleConfirmPayment(item)}
-                          onConfirmedBillOfSale={() =>
-                            handleConfirmBillOfSale(item)
-                          }
-                        />
+              {filteredBillingRecords
+                .sort((a, b) => new Date(a.month) - new Date(b.month))
+                .slice(0, isTableExpanded ? filteredBillingRecords.length : 4)
+                .map((item, index) => (
+                  <tr
+                    key={index}
+                    className="relative text-center border-t border-gray-300"
+                  >
+                    <td className="text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-auto py-2">
+                      {item.month
+                        ? `${monthsInRangeShort[parseInt(item.month.split("-")[1], 10) - 1]}/${String(selectedYear).slice(-2)}`
+                        : "Mês inválido"}
+                    </td>
+                    <td className="text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
+                      R${" "}
+                      {parseFloat(item.consultation_fee)
+                        .toFixed(2)
+                        .replace(".", ",")}
+                    </td>
+                    <td className="hidden md:table-cell text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
+                      {item.consultation_days
+                        ? item.consultation_days
+                            .split(", ")
+                            .map(Number)
+                            .sort((a, b) => a - b)
+                            .join(", ")
+                        : "-"}
+                    </td>
+                    <td className="relative text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2 group">
+                      {item.num_consultations}
+                    </td>
+                    <td className="text-center text-texto1 md:text-F15 text-F8 font-normal font-['Open Sans'] tracking-tight px-2 md:px-4 py-1 md:py-2">
+                      R$ {item.total_consultation_fee || "0,00"}
+                    </td>
+                    <td>
+                      <div className="flex items-center justify-center text-center h-full">
+                        {item.sending_invoice ? (
+                          <VerifyGreenIcon />
+                        ) : (
+                          <CrossIcon />
+                        )}
                       </div>
-                    )}
-                  </td>
-                </tr>
-              ))}
+                    </td>
+                    <td>
+                      <div className="flex items-center justify-center text-center h-full">
+                        {" "}
+                        {item.payment_status === "pago" ? (
+                          <VerifyGreenIcon />
+                        ) : item.payment_status === "parcial" ? (
+                          <span className="text-texto2 md:text-F15 text-F8 font-semibold font-['Open Sans'] tracking-tight rounded-B15 border-2 border-aviso">
+                            R${" "}
+                            {parseFloat(item.payment_amount || 0)
+                              .toFixed(2)
+                              .replace(".", ",")}
+                          </span>
+                        ) : (
+                          <CrossIcon />
+                        )}{" "}
+                      </div>
+                    </td>
+                    <td>
+                      <div className="flex items-center justify-center text-center h-full">
+                        {item.bill_of_sale ? (
+                          <VerifyGreenIcon />
+                        ) : (
+                          <CrossIcon />
+                        )}
+                      </div>
+                    </td>
+
+                    <td className="text-center px-2 md:px-4 py-1 md:py-2">
+                      <button
+                        className="cursor-pointer"
+                        onClick={() => toggleDropdownPatients(index)}
+                      >
+                        <HamburguerIcon />
+                      </button>
+
+                      {isDropdownOpenPatients === index && (
+                        <div
+                          ref={outSideClickRef}
+                          className="absolute -mt-23 ml-40 shadow-lg rounded z-20"
+                        >
+                          <DropDownDashActions
+                            onOpenModal={() => handleOpenModalForBilling(item)}
+                            onPartialPayment={() =>
+                              handleOpenPartialPayment(item)
+                            }
+                            onConfirmedPayment={() =>
+                              handleConfirmPayment(item)
+                            }
+                            onConfirmedBillOfSale={() =>
+                              handleConfirmBillOfSale(item)
+                            }
+                          />
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
             <tfoot>
               <tr>
-                <td colSpan="8" className="relative py-3">
-                  <div
-                    className={`flex justify-center items-center relative w-full transition-all duration-300 md:mt-4 ${isTableExpanded ? "h-auto" : "h-screen"}`}
-                  >
+                <td colSpan="9" className="relative py-3">
+                  <div className="flex justify-center items-center relative w-full transition-all duration-300 md:mt-4">
                     <button
                       onClick={toggleTableSize}
-                      className={`absolute transform  cursor-pointer transition-transform duration-300 ${
-                        isTableExpanded
-                          ? "rotate-0 bottom-0"
-                          : "rotate-180 bottom-5"
+                      className={`absolute transform cursor-pointer transition-transform duration-300 ${
+                        isTableExpanded ? "rotate-180" : "rotate-0"
                       }`}
                     >
-                      <div className="md:w-[452px] w-[263px]  h-[1px] bg-cinza6 absolute top-[-20px] left-1/2 transform -translate-x-1/2 mt-3 "></div>
+                      <div className="md:w-[452px] w-[263px] h-[1px] bg-cinza6 absolute top-[-20px] left-1/2 transform -translate-x-1/2 mt-3"></div>
                       <ArrowDownIcon />
                     </button>
                   </div>
