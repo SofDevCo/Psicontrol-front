e886ae368f0e8d1ba7e661dfcc91286b6ebaad1a
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-02-07 17:03:53 -0300

    fix(pages):"font removed "

--- src/utils/Months/months.js
+++ src/utils/Months/months.js
@@ -74,7 +74,7 @@ export const Months = ({
       >
         <div className="flex items-center gap-2">
           <VerifyIcon />
-          <span className="text-texto4 md:text-[21px] text-[15px] md:font-medium md:font-['Ubuntu'] font-semibold  font-['Inter'] md:tracking-tight tracking-wider">
+          <span className="text-texto4 md:text-[21px] text-[15px] md:font-medium  font-semibold   md:tracking-tight tracking-wider">
             <span className="block md:hidden">{`${monthsInRangeShort[selectedMonth - 1]}/${selectedYear.toString().slice(-2)}`}</span>
             <span className="hidden md:block">{`${monthsInRange[selectedMonth - 1]}/${selectedYear.toString().slice(-2)}`}</span>
           </span>
