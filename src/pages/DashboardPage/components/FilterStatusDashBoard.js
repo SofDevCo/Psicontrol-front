29c813a18abb7129838d13ab85577a4552f1470c
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-02-10 10:18:07 -0300

    feat(pages):"button for filter status in dashboard"

--- src/pages/DashboardPage/components/FilterStatusDashBoard.js
+++ src/pages/DashboardPage/components/FilterStatusDashBoard.js
@@ -3,13 +3,19 @@ import { useState } from "react";
 
 const FilterStatusDashBoard = ({ selectedStatus, onChangeStatus }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState(selectedStatus || []);
 
   const handleCheckboxChange = (status) => {
-    if (selectedStatus.includes(status)) {
-      onChangeStatus(selectedStatus.filter((item) => item !== status));
-    } else {
-      onChangeStatus([...selectedStatus, status]);
-    }
+    setSelectedFilter((prev) =>
+      prev.includes(status)
+        ? prev.filter((item) => item !== status)
+        : [...prev, status]
+    );
+  };
+
+  const apllyFIlter = () => {
+    onChangeStatus(selectedFilter);
+    setIsOpen(false);
   };
 
   return (
@@ -31,9 +37,10 @@ const FilterStatusDashBoard = ({ selectedStatus, onChangeStatus }) => {
               <input
                 type="checkbox"
                 id="aberto"
-                checked={selectedStatus.includes("aberto")}
+                checked={selectedFilter.includes("aberto")}
                 onChange={() => handleCheckboxChange("aberto")}
-                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"/>
+                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"
+              />
               <label className="ml-2" htmlFor="aberto">
                 Não confirmados
               </label>
@@ -42,9 +49,10 @@ const FilterStatusDashBoard = ({ selectedStatus, onChangeStatus }) => {
               <input
                 type="checkbox"
                 id="pago"
-                checked={selectedStatus.includes("pago")}
+                checked={selectedFilter.includes("pago")}
                 onChange={() => handleCheckboxChange("pago")}
-                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"/>
+                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"
+              />
               <label className="ml-2" htmlFor="pago">
                 Confirmados
               </label>
@@ -53,9 +61,10 @@ const FilterStatusDashBoard = ({ selectedStatus, onChangeStatus }) => {
               <input
                 type="checkbox"
                 id="parcial"
-                checked={selectedStatus.includes("parcial")}
+                checked={selectedFilter.includes("parcial")}
                 onChange={() => handleCheckboxChange("parcial")}
-                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"/>
+                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"
+              />
               <label className="ml-2" htmlFor="parcial">
                 Parciais
               </label>
@@ -68,9 +77,10 @@ const FilterStatusDashBoard = ({ selectedStatus, onChangeStatus }) => {
               <input
                 type="checkbox"
                 id="nao-realizada"
-                checked={selectedStatus.includes("nao-realizada")}
+                checked={selectedFilter.includes("nao-realizada")}
                 onChange={() => handleCheckboxChange("nao-realizada")}
-                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"/>
+                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"
+              />
               <label className="ml-2" htmlFor="nao-realizada">
                 Não Realizada
               </label>
@@ -79,13 +89,48 @@ const FilterStatusDashBoard = ({ selectedStatus, onChangeStatus }) => {
               <input
                 type="checkbox"
                 id="realizada"
-                checked={selectedStatus.includes("realizada")}
+                checked={selectedFilter.includes("realizada")}
                 onChange={() => handleCheckboxChange("realizada")}
-                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none mt-1 border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"/>
+                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none mt-1 border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"
+              />
               <label className="ml-2" htmlFor="realizada">
                 Realizada
               </label>
             </div>
+            <p className="md:mt-4 mt-2 md:w-[93px] text-texto2 md:text-[17px] text-[10px] font-normal font-['Open Sans'] tracking-tight underline ">
+              Recibos
+            </p>
+            <div className="flex items-center md:w-auto text-texto2 md:text-[15px] text-[10px] font-normal font-['Open Sans'] tracking-tight md:mt-1 mt-2">
+              <input
+                type="checkbox"
+                id="nao-emitidos"
+                checked={selectedFilter.includes("nao-emitidos")}
+                onChange={() => handleCheckboxChange("nao-emitidos")}
+                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"
+              />
+              <label className="ml-2" htmlFor="nao-emitidos">
+                Não Emitidos
+              </label>
+            </div>
+            <div className="flex items-center md:w-auto text-texto2 md:text-[15px] text-[10px] font-normal font-['Open Sans'] tracking-tight md:mt-0 mt-1">
+              <input
+                type="checkbox"
+                id="emitidos"
+                checked={selectedFilter.includes("emitidos")}
+                onChange={() => handleCheckboxChange("emitidos")}
+                className="w-[11.45px] h-[11.44px] md:w-[16px] md:h-[16px] appearance-none mt-1 border-2 border-primaria rounded-sm bg-white checked:bg-primaria checked:border-primaria inline-flex cursor-pointer relative checked:after:content-['✔'] checked:after:text-white checked:after:absolute checked:after:top-[-4px] checked:after:left-[0px] checked:after:font-bold"
+              />
+              <label className="ml-2 mt-1" htmlFor="emitidos">
+                Emitidos
+              </label>
+            </div>
+            <button
+              type="submit"
+              onClick={apllyFIlter}
+              className="flex mt-3 mx-auto md:h-6 px-6 py-0.5 bg-primaria rounded-[100px] text-white text-sm font-semibold  shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30)]"
+            >
+              Filtrar
+            </button>
           </div>
         )}
       </div>
