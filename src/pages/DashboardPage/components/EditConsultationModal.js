26d4b95621e2c8b7f367522646269ad468e80d35
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-02-21 10:57:02 -0300

    feat(pages):"Labels days for edit consultation modal "

--- src/pages/DashboardPage/components/EditConsultationModal.js
+++ src/pages/DashboardPage/components/EditConsultationModal.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from "react";
+import {
+  EditIcon,
+  CloseMiniIcon,
+  AddConsultationIcon,
+} from "../../CustomerPage/components/IconsRegisterCard";
 
 const EditConsultationModal = ({
   isOpen,
@@ -9,6 +14,7 @@ const EditConsultationModal = ({
 }) => {
   const [days, setDays] = useState([]);
   const [newDay, setNewDay] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     console.log("Modal recebeu o patient:", patient);
@@ -26,56 +32,74 @@ const EditConsultationModal = ({
     const dayTrimmed = newDay.trim();
     if (dayTrimmed && !days.includes(dayTrimmed)) {
       setDays((prev) => [...prev, dayTrimmed]);
-
       onAddDay(patient.customer_id, dayTrimmed);
-
       setNewDay("");
     }
   };
 
   const handleRemoveDayLocal = (dayToRemove) => {
     setDays((prev) => prev.filter((d) => d !== dayToRemove));
-
     onRemoveDay(patient.customer_id, dayToRemove);
   };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-        <h2 className="text-lg font-bold mb-4">
-          Editar Consultas - {patient.Customer?.customer_name || "Sem nome"}
-        </h2>
-        <div className="flex flex-wrap gap-2 mb-4">
-          {days.map((day, index) => (
-            <div
-              key={index}
-              className="flex items-center bg-gray-200 px-3 py-1 rounded-full"
-            >
-              <span className="mr-2">{day}</span>
-              <button
-                onClick={() => handleRemoveDayLocal(day)}
-                className="text-red-500 font-bold"
-              >
-                ×
-              </button>
+      <div className="bg-neutral-100 w-[437px] h-[341px] p-6 rounded-lg shadow-lg border border-cinza6">
+        <h1 className="text-center text-primaria text-[21px] font-medium tracking-tight mb-7">
+          Editar Consultas
+        </h1>
+        <span className="text-texto1 text-[15px] font-semibold tracking-tight">
+          Paciente:
+        </span>
+        <br />
+        <span className="text-texto1 text-[15px] font-normal tracking-tight">
+          {patient.Customer?.customer_name || "Sem nome"}
+        </span>
+        <div>
+          <div className="flex flex-wrap gap-2 mb-4">
+            <span className="text-texto1 text-[15px] font-semibold tracking-tight">
+              Dias de consulta
+            </span>
+            <div className="flex flex-wrap gap-2">
+              {days.map((day, index) => (
+                <div
+                  key={index}
+                  className="relative flex flex-col items-center w-[43px] h-auto p-2 rounded-[15px] border-2 border-cinza6"
+                >
+                  <span className="mb-1">{day}</span>
+                  {isEditing && (
+                    <button
+                      onClick={() => handleRemoveDayLocal(day)}
+                      className="absolute -bottom-5"
+                    >
+                      <CloseMiniIcon />
+                    </button>
+                  )}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="flex gap-2">
-          <input
-            type="number"
-            placeholder="Novo dia"
-            value={newDay}
-            onChange={(e) => setNewDay(e.target.value)}
-            className="border px-3 py-1 rounded w-full"
-          />
-          <button
-            onClick={handleAddDayLocal}
-            className="bg-blue-500 text-white px-3 py-1 rounded"
-          >
-            +
+          </div>
+          <button onClick={() => setIsEditing(!isEditing)}>
+            {isEditing ? <CloseMiniIcon /> : <EditIcon />}
           </button>
         </div>
+        {isEditing && (
+          <div className="flex gap-2 mt-2">
+            <input
+              type="number"
+              placeholder="Novo dia"
+              value={newDay}
+              onChange={(e) => setNewDay(e.target.value)}
+              className="border-2 border-cinza6 px-3 py-1 rounded-[15px] w-[43px] h-auto"
+            />
+            <button
+              onClick={handleAddDayLocal}
+              className="bg-blue-500 text-white px-3 py-1 rounded"
+            >
+              +
+            </button>
+          </div>
+        )}
         <div className="mt-4 flex justify-end">
           <button onClick={onClose} className="bg-gray-300 px-4 py-2 rounded">
             Fechar
