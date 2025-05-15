3838403fe8022015fe7b46016191e411a8328e28
unknown <jcpouey@gmail.com>
2025-05-15 07:42:24 -0300

    style/adjusting buttons alignment, underline and title centralization

--- src/pages/DashboardPage/components/BillingDashBoard.js
+++ src/pages/DashboardPage/components/BillingDashBoard.js
@@ -13,7 +13,7 @@ const BillingDashBoard = ({
     <div className="fixed inset-0 bg-bgM/30 bg-opacity-50 backdrop-blur-sm flex justify-center items-start z-30">
       <div className="bg-bg1 rounded-B15 p-6 w-[90%] sm:w-[400px] lg:w-[477px] max-h-[90vh] lg:h-[521px] border border-cinza6 lg:border-[3px] mt-[18vh] flex flex-col">
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-center text-primaria lg:text-[21px] text-sm font-medium font-ubuntu tracking-tight lg:ml-16 lg:mt-3">
+          <h2 className="text-center text-primaria lg:text-[21px] text-sm font-medium font-ubuntu tracking-tight lg:ml-[74px] lg:mt-3">
             Enviar mensagem de cobrança
           </h2>
           <button onClick={onClose}>
@@ -30,16 +30,16 @@ const BillingDashBoard = ({
         </div>
 
         <div className="border-b border-cinza6 mb-4"></div>
-        <div className="flex flex-col lg:flex-row justify-between gap-4 lg:ml-8">
+        <div className="flex flex-col lg:flex-row justify-between gap-4">
           <button
             onClick={onSendEmail}
-            className="w-full h-auto px-4 py-2 bg-bg1 rounded-[100px] border border-primaria text-primaria text-sm font-semibold text-center lg:text-sm text-[10px]"
+            className="w-full h-auto px-4 py-2 bg-bg1 rounded-[100px] border border-primaria text-primaria text-sm font-semibold text-center underline lg:text-sm text-[10px]"
           >
             Enviar por Email
           </button>
           <button
             onClick={onSendWhatsApp}
-            className="w-full h-auto px-4 py-2 bg-bg1 rounded-[100px] border border-primaria text-primaria text-sm font-semibold text-center lg:text-sm text-[10px]"
+            className="w-full h-auto px-4 py-2 bg-bg1 rounded-[100px] border border-primaria text-primaria text-sm font-semibold text-center underline lg:text-sm text-[10px]"
           >
             Enviar pelo WhatsApp
           </button>
